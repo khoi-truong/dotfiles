c62004a3b94d3d3fade8d2889436b9c3a5eb6678
Khoi Truong Minh <khoi.truongminh@gmail.com>
2020-08-14 00:02:16 +0700

    Update setup.sh to use `for` loop and cleanup brew bundle

--- setup.sh
+++ setup.sh
@@ -1,30 +1,30 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/zsh
 
-# Install Homebrew casks and setup macos defaults
+# Install Homebrew casks
+echo "Homebrew Bundle..."
 cd ./macos
 brew bundle
-chmod +x ./setup.sh
-sudo ./setup.sh
+brew bundle cleanup -f
 cd ..
 
-DOTFILES="$(cd "$(dirname "$0")"; pwd)";
+# Setup macOS defaults
+execute() {
+    chmod +x "$1"; "$1";
+} 
 
-# Setup Terminal
-
-chmod +x ./terminal/setup.sh
-sudo ./terminal/setup.sh
-
-# Setup iTerm
-chmod +x ./iterm/setup.sh
-sudo ./iterm/setup.sh
+for file in ./{macos,terminal,iterm}/setup.sh; do
+	[ -r "$file" ] && [ -f "$file" ] && execute "$file"
+done;
+unset file;
+unset execute;
 
 # Make symbolic links
-
+echo "Link configs to ${HOME}"
+DOTFILES="$(cd "$(dirname "$0")"; pwd)";
 ln -fs "${DOTFILES}/zsh/zshrc" ~/.zshrc
 ln -fs "${DOTFILES}/git/gitconfig" ~/.gitconfig
 ln -fs "${DOTFILES}/git/gitignore" ~/.gitignore
 ln -fs "${DOTFILES}/ssh/config" ~/.ssh/config
 
 # Remove redundant history file
-
 rm ~/.zsh_history 2> /dev/null
