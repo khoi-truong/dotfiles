1324147f761ce2afbe99d70ea7f60f76379ff84c
Khoi Truong Minh <khoi.truongminh@gmail.com>
2020-08-16 14:29:52 +0700

    Add GoTiengViet setup files

---
+++ misc/gotiengviet/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Change GoTiengViet menu bar icons"
+CURRENT_DIR="$(cd "$(dirname "$0")"; pwd)";
+for icon in {E,E@2x,V,V@2x}; do
+    cp -f "${CURRENT_DIR}/Resources/${icon}.png" "/Applications/GoTiengViet.app/Contents/Resources/${icon}.png"
+done
--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ execute() {
     chmod +x "$1"; "$1";
 }
 
-for file in ./{macos,terminal,iterm,alfred}/setup.sh; do
+for file in ./{macos,terminal,iterm,alfred,misc/gotiengviet}/setup.sh; do
 	[ -r "$file" ] && [ -f "$file" ] && execute "$file"
 done;
 unset file;
