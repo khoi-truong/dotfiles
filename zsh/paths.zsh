782fd61e43205eb22ab0a26e5f57fc995d2ee8f7
Khoi Truong Minh <khoi.truongminh@gmail.com>
2020-09-22 16:10:45 +0700

    Add cmake, gh, llvm, node, rustup, yarn

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,7 +1,8 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-export GOPATH=$HOME/Development/go
-export GOBIN=$GOPATH/bin
+export GOPATH="$HOME/Development/go"
+export GOBIN="$GOPATH/bin"
 export PATH=$PATH:$GOPATH
 export PATH=$PATH:$GOBIN
+export PATH=$PATH:"$HOME/.cargo/bin"
