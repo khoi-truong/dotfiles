3ca77e325d6f34076275e89bd1d73bb97455f48a
Khoi Truong Minh <khoi.truongminh@gmail.com>
2020-10-29 14:58:19 +0700

    Brew install carthage

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,8 +1,9 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-export GOPATH="$HOME/Development/go"
+export GOPATH="$HOME/Code/go"
 export GOBIN="$GOPATH/bin"
 export PATH=$PATH:$GOPATH
 export PATH=$PATH:$GOBIN
 export PATH=$PATH:"$HOME/.cargo/bin"
+export PATH=$PATH:"$HOME/.config/carthage/bin"
