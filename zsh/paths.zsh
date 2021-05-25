a85f9dc0d5fddf54995354818313dd0466b0f44d
Khoi Truong Minh <khoi.truongminh@gmail.com>
2021-05-25 13:21:57 +0700

    Fix pyenv 2.0.0 path and nvm completion

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -5,5 +5,11 @@ export GOPATH="$HOME/Code/go"
 export GOBIN="$GOPATH/bin"
 export PATH=$PATH:$GOPATH
 export PATH=$PATH:$GOBIN
+
 export PATH=$PATH:"$HOME/.cargo/bin"
+
 export PATH=$PATH:"$HOME/.config/carthage/bin"
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH=$PATH:"$PYENV_ROOT/bin"
+eval "$(pyenv init --path)"
