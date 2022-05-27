565ecc45dd138a1cc171154a3c12bbf174ffa950
Khoi Truong <khoi.truongminh@gmail.com>
2022-05-27 17:35:02 +0700

    feat: remove .zsh_sessions and setup z

--- setup.sh
+++ setup.sh
@@ -23,6 +23,7 @@ unset execute;
 # Make symbolic links
 echo ""
 echo "Link configs to ${HOME}"
+ln -fs "${DOTFILES}/zsh/zshenv" ~/.zshenv
 ln -fs "${DOTFILES}/zsh/zshrc" ~/.zshrc
 ln -fs "${DOTFILES}/git/gitconfig" ~/.gitconfig
 ln -fs "${DOTFILES}/git/gitignore" ~/.gitignore
@@ -30,6 +31,7 @@ ln -fs "${DOTFILES}/ssh/config" ~/.ssh/config
 
 # Remove redundant history file
 rm ~/.zsh_history 2> /dev/null
+rm -rf ~/.zsh_sessions 2> /dev/null
 
 echo ""
 echo "Setting up completed!"
