711852273f72fd5006a6157d6fff544688530e40
Khoi Truong <khoi.truongminh@gmail.com>
2022-06-29 14:38:42 +0700

    feat: keep sudo until setup complete

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 
 DOTFILES="$(cd "$(dirname "$0")"; pwd)";
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.setup.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install Homebrew casks
 echo "Homebrew Bundle..."
 cd ${DOTFILES}/brew
